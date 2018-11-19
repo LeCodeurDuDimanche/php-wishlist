7374ca503bec4c403210c60c936c12a2162739d7
Adrien Duval <adrien.duval.bonnet@gmail.com>
2018-11-19 15:53:08 +0100

    Ajout test.php

--- test.php
+++ test.php
@@ -1,6 +1,17 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use \mywishlist\models\Item;
 
-$i = new Item;
+use \mywishlist\modele\Item;
+use Illuminate\Database\Capsule\Manager as DB;
+$db = new DB();
+$db->addConnection(parse_ini_file("src/conf/conf.ini"));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+foreach(Item::select("*")->get() as $i)
+{
+    echo "<p>";
+    var_dump($i);
+    echo "</p>";
+}
