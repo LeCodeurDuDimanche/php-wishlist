a226bb0abf0105d9f9849350207c3a89a39aa652
Thibaut Vagner <thibaut.vagner2@etu.univ-lorraine.fr>
2018-11-21 08:28:42 +0100

    test

--- test.php
+++ test.php
@@ -3,6 +3,7 @@ require_once 'vendor/autoload.php';
 
 
 use \mywishlist\modele\Item;
+use \mywishlist\modele\Liste;
 use Illuminate\Database\Capsule\Manager as DB;
 $db = new DB();
 $db->addConnection(parse_ini_file("src/conf/conf.ini"));
@@ -14,4 +15,9 @@ foreach(Item::select("*")->get() as $i)
     echo "<p>";
     var_dump($i);
     echo "</p>";
+}
+
+$lSouhait = Liste::select('*')->get();
+foreach($lSouhait as $i){
+	echo "<p>".$i."</p>";
 }
