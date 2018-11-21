8874bef04605da35cb5492d7fd8981a17ff663fd
Adrien Duval <adrien.duval.bonnet@gmail.com>
2018-11-21 09:18:41 +0100

    Changement namespace

--- src/models/Liste.php
+++ src/models/Liste.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace mywishlist\modele;
+namespace mywishlist\models;
 
 class Liste extends \illuminate\Database\Eloquent\Model{
 
--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use \mywishlist\modele\Item;
-use \mywishlist\modele\Liste;
+use \mywishlist\models\Item;
+use \mywishlist\models\Liste;
 use Illuminate\Database\Capsule\Manager as DB;
 $db = new DB();
 $db->addConnection(parse_ini_file("src/conf/conf.ini"));
