a1827f42b8a1fc6f30046234957afb912ac18ee3
Thibaut Vagner <thibaut.vagner2@etu.univ-lorraine.fr>
2018-12-12 17:59:57 +0100

    ajout formulaire modification d'item dans une liste

--- src/controleurs/ControleurListeCreateur.php
+++ src/controleurs/ControleurListeCreateur.php
@@ -4,6 +4,7 @@
 
  use mywishlist\models\Liste;
 
+ use mywishlist\models\Item;
 
  class ControleurListeCreateur extends Controleur{
 
@@ -21,15 +22,17 @@
 
      public function afficherFormulaireAjoutItem($request, $response, $args)
      {
-  		$liste = Liste::find($args['id']);
+        $idItem = filter_var($args['id'], FILTER_SANITIZE_STRING);
+  		$liste = Liste::find($idItem);
         return $this->view->render($response, "createur/ajouterItem.html", compact("liste"));
      }
-/*
+
 	 public function afficherModifItemListe($request, $response, $args)
      {
-  		
-        return $this->view->render($response, "createur/modifierItem.html", compact("liste"));
-     }  */   
+  		$liste = Liste::where('id', '=', $args['id'])->get()[0];
+        $item = Item::where('id', '=', $args['num'])->first();
+        return $this->view->render($response, "createur/modifierItem.html", compact("liste", "item"));
+     }   
 
  	public function afficherListe($request, $response, $args){
  		$liste = Liste::find($args['id']);
