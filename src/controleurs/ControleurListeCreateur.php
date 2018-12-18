c6ac0c5db88d3536da67d698a21e27068d41047c
Thibaut Vagner <thibaut.vagner2@etu.univ-lorraine.fr>
2018-12-18 15:24:34 +0100

    ajouterItem

--- src/controleurs/ControleurListeCreateur.php
+++ src/controleurs/ControleurListeCreateur.php
@@ -27,6 +27,24 @@
         return $this->view->render($response, "createur/ajouterItem.html", compact("liste"));
      }
 
+     public function ajouterItem($request, $response, $args)
+     {
+        $titre = filter_var($_POST["nom"], FILTER_SANITIZE_STRING);
+        $descrip = filter_var($_POST["desc"], FILTER_SANITIZE_STRING);
+        $img = filter_var($_POST["image"], FILTER_SANITIZE_STRING);
+        $url = filter_var($_POST["url"], FILTER_SANITIZE_STRING);
+        $prix = filter_var($_POST["tarif"], FILTER_SANITIZE_STRING);
+        $idListe = filter_var($args['id'],FILTER_SANITIZE_STRING );
+        $item = new Item();
+        $item->titre = $titre;
+        $item->desc = $descrip;
+        $item->img = $img;
+        $item->url = $url;
+        $item->tarif = $prix;
+        $item->save();
+        return $response->withRedirect($this->router->pathFor('listeCreateurDetails'));
+     }
+
 	 public function afficherModifItemListe($request, $response, $args)
      {
   		$liste = Liste::where('id', '=', $args['id'])->get()[0];
