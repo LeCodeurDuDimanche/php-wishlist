010379f92b2d6e8a01f488d2fe7a757fb1def02e
Antoine Pinot <antoine.pinot1@etu.univ-lorraine.fr>
2018-12-18 11:15:39 +0100

    ajout d'une page d'erreur lors d'un token participant erroné

--- src/controleurs/ControleurListeParticipant.php
+++ src/controleurs/ControleurListeParticipant.php
@@ -8,14 +8,22 @@
 
  	public function afficherListe($request, $response, $args){
  		$token = filter_var($args['token'], FILTER_SANITIZE_STRING);
-        $liste = Liste::where('tokenParticipant', '=', $token)->first();
+        $liste = $this->recupererListe($request, $response, $token);
  		return $this->view->render($response, "participant/affichageListe.html", ["liste" => $liste]);
  	}
 
  	public function afficherListeAvecDetails($request, $response, $args){
  		$token = filter_var($args['token'], FILTER_SANITIZE_STRING);
- 		$liste = Liste::where('tokenParticipant', '=', $token)->first();
+ 		$liste = $this->recupererListe($request, $response, $token);
  		$listeIt = $liste->items()->get();
  		return $this->view->render($response, "participant/affichageListeDetails.html", ["liste" => $liste , "listeIt" => $listeIt]);
+ 	}
+
+ 	private function recupererListe($request, $response, $token){
+ 		$liste = Liste::where('tokenParticipant', '=', $token)->first();
+ 		if(empty($liste)){
+ 			throw new \Slim\Exception\NotFoundException($request, $response);
+ 		}
+ 		return $liste;
  	}
  }
