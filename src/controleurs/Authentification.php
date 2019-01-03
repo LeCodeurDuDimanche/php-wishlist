88641382b59dad51a65480088891058fabe9455c
Adrien Duval <adrien.duval.bonnet@gmail.com>
2019-01-03 19:49:45 +0100

    Correction authentification et ajout bouton login dans la navbar

--- src/controleurs/Authentification.php
+++ src/controleurs/Authentification.php
@@ -5,25 +5,26 @@
 
     class Authentification {
 
-        public function creerCompte(string $nom, string $mdp) : integer
+        public static function creerCompte(string $nom, string $mdp) : int
         {
             $u = new Utilisateur();
             $u->nom = $nom;
-            $u->mdp = password_hash($mdp);
+            var_dump($mdp, password_hash($mdp, PASSWORD_DEFAULT));
+            $u->mdp = password_hash($mdp, PASSWORD_DEFAULT);
             $id = $u->save();
 
-            if (! connexion($nom, $mdp))
-                $id = false;
+            if (! static::connexion($nom, $mdp))
+                $id = -1;
 
             return $id;
         }
 
-        public function connexion(string $nom, string $mdp) : boolean
+        public static function connexion(string $nom, string $mdp) : bool
         {
             if (session_status() != PHP_SESSION_ACTIVE)
                 session_start();
 
-            if ($this->estConnecte())
+            if (static::estConnecte())
                 return true;
 
             $user = Utilisateur::where("nom", "=", $nom)->first();
@@ -36,25 +37,25 @@
             return false;
         }
 
-        public function deconnexion()
+        public static function deconnexion()
         {
             unset($_SESSION['user']);
         }
 
-        public function getNomUtilisateur() : string
+        public static function getNomUtilisateur() : string
         {
             return $_SESSION['user']['nom'];
         }
 
-        public function getUtilisateur() : Utilisateur
+        public static function getUtilisateur() : Utilisateur
         {
-            if (!$this->estConnecte())
+            if (!static::estConnecte())
                 return null;
 
             return Utilisateur::where("id", "=", $_SESSION['user']['id'])->first();
         }
 
-        public function estConnecte() : boolean
+        public static function estConnecte() : bool
         {
             return isset($_SESSION['user']);
         }
