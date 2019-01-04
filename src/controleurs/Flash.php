e00e9db3fbc281cd03200d4a617b56cf6e8e4e2d
Adrien Duval <adrien.duval.bonnet@gmail.com>
2019-01-04 17:45:19 +0100

    Correction classe Flash

--- src/controleurs/Flash.php
+++ src/controleurs/Flash.php
@@ -6,6 +6,8 @@
 
     class Flash {
 
+        private static $flashedData;
+
         private static function init()
         {
             if (session_status() != PHP_SESSION_ACTIVE)
@@ -15,9 +17,8 @@
             {
                 $_SESSION['flash'] = [];
             }
-
-            var_dump("INIT");
-            var_dump($_SESSION["flash"]);
+            if (self::$flashedData === null)
+                self::$flashedData = [];
         }
 
 
@@ -28,26 +29,29 @@
 
         public static function get($name) {
             self::init();
-            return has($name) ? $_SESSION["flash"][$name] : null;
+            return self::has($name) ? $_SESSION["flash"][$name] : null;
         }
 
         public static function flash($key, $value)
         {
             self::init();
-            $_SESSION['flash'][$key] = $value;
+            self::$flashedData[$key] = $value;
         }
 
         public static function reset()
         {
             self::init();
-            $_SESSION['flash'] = [];
+            $_SESSION['flash'] = self::$flashedData;
+            self::$flashedData = [];
         }
 
         public static function middleware() : callable
         {
-            return function(ServerRequestInterface $request, ResponseInterface $response, callable $next){
+            return function(ServerRequestInterface $request, ResponseInterface $response, callable $next) : ResponseInterface
+            {
+                $response = $next($request, $response);
                 Flash::reset();
-                return $next($request, $response);
+                return $response;
             };
         }
 
